6ff21ffe45809f87515bc98b66f65587f04cf893
SnappleCap <glennowo@buffalo.edu>
2015-03-08 23:48:57 -0400

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,4 +1,18 @@
-document.addEventListener("deviceready", onDeviceReady, false);
+function init() {
+	document.addEventListener("deviceready", deviceReady, true);
+	delete init;
+}
+
+function checkPreAuth() {
+	console.log("checkPreAuth");
+    var form = $("#loginForm");
+    if(window.localStorage["username"] != undefined && window.localStorage["password"] != undefined && window.localStorage["dev"] != undefined) {
+        $("#username", form).val(window.localStorage["username"]);
+        $("#password", form).val(window.localStorage["password"]);
+		$("#dev", form).val(window.localStorage["dev"]);
+        handleLogin();
+    }
+}
 
 function handleLogin() {
     var form = $("#loginForm");    
@@ -6,25 +20,33 @@ function handleLogin() {
     $("#submitButton",form).attr("disabled","disabled");
     var u = $("#username", form).val();
     var p = $("#password", form).val();
-    var d = $("dev", form).val();
-    console.log("click");
+	var d = $("#dev", form).val();
     if(u != '' && p!= '') {
         $.post("http://democv.softrek.com:8082/clearview-api", {username:u,password:p,dev:d}, function(res) {
             if(res == true) {
                 //store
                 window.localStorage["username"] = u;
                 window.localStorage["password"] = p;
-                window.localStorage["dev"] = d;
-                $.mobile.changePage("some.html");
+				window.localStorage["dev"] = d;
+                //$.mobile.changePage("some.html");
             } else {
                 navigator.notification.alert("Your login failed", function() {});
             }
          $("#submitButton").removeAttr("disabled");
         },"json");
     } else {
-        //Thanks Igor!
-        navigator.notification.alert("You must enter a username, password and dev", function() {});
+        navigator.notification.alert("You must enter a username and password and dev", function() {});
         $("#submitButton").removeAttr("disabled");
     }
     return false;
+}
+
+function deviceReady() {
+	console.log("deviceReady");
+	$("#loginPage").on("pageinit",function() {
+		console.log("pageinit run");
+		$("#loginForm").on("submit",handleLogin);
+		checkPreAuth();
+	});
+	$.mobile.changePage("#loginPage");
 }
